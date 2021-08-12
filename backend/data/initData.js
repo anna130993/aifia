e1625c5cd64b6b562fc5fb882e4c11e8729ee77b
Anna Podsiadlo <annamagdalena.podsiadlo@gmail.com>
2021-08-12 21:35:42 +0200

    Switch to locally sourced images

--- backend/data/initData.js
+++ backend/data/initData.js
@@ -8,19 +8,19 @@ const loadInitData = async() => {
       startPrice: 50,
       photos: [{
         name: 'Base - white t-shirt',
-        src: 'https://images.pexels.com/photos/8148576/pexels-photo-8148576.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        src: '/images/white-ts.jpeg',
       },{
         name: 'Fragonard - Swing',
-        src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Joean_Honor%C3%A9_Fragonard_-_The_Swing.jpg/800px-Joean_Honor%C3%A9_Fragonard_-_The_Swing.jpg',
+        src: '/images/fragonard.jpeg',
       },{
         name: 'Van Gogh - Starry Night',
-        src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/1280px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg',
+        src: '/images/vangogh.jpeg',
       },{
         name: 'Botticelli - Spring',
-        src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Botticelli-primavera.jpg/1280px-Botticelli-primavera.jpg',
+        src: '/images/botticelli.jpeg',
       },{
         name: 'Delacroix - Liberty Leading the People',
-        src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Eug%C3%A8ne_Delacroix_-_Le_28_Juillet._La_Libert%C3%A9_guidant_le_peuple.jpg/1280px-Eug%C3%A8ne_Delacroix_-_Le_28_Juillet._La_Libert%C3%A9_guidant_le_peuple.jpg',
+        src: '/images/delacroix.jpeg',
       }],
     },{
       name: 'Shirt prints',
@@ -28,19 +28,19 @@ const loadInitData = async() => {
       startPrice: 75,
       photos: [{
         name: 'Base - white shirt',
-        src: 'https://images.pexels.com/photos/3756679/pexels-photo-3756679.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        src: '/images/white-shirt.jpeg',
       },{
         name: 'Fragonard - Swing',
-        src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Joean_Honor%C3%A9_Fragonard_-_The_Swing.jpg/800px-Joean_Honor%C3%A9_Fragonard_-_The_Swing.jpg',
+        src: '/images/fragonard.jpeg',
       },{
         name: 'Van Gogh - Starry Night',
-        src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/1280px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg',
+        src: '/images/vangogh.jpeg',
       },{
         name: 'Botticelli - Spring',
-        src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Botticelli-primavera.jpg/1280px-Botticelli-primavera.jpg',
+        src: '/images/botticelli.jpeg',
       },{
         name: 'Delacroix - Liberty Leading the People',
-        src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Eug%C3%A8ne_Delacroix_-_Le_28_Juillet._La_Libert%C3%A9_guidant_le_peuple.jpg/1280px-Eug%C3%A8ne_Delacroix_-_Le_28_Juillet._La_Libert%C3%A9_guidant_le_peuple.jpg',
+        src: '/images/delacroix.jpeg',
       }],
     },{
       name: 'Dress prints',
@@ -48,19 +48,19 @@ const loadInitData = async() => {
       startPrice: 99,
       photos: [{
         name: 'Base - white dress',
-        src: 'https://images.pexels.com/photos/6163979/pexels-photo-6163979.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        src: '/images/white-dress.jpeg',
       },{
         name: 'Fragonard - Swing',
-        src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Joean_Honor%C3%A9_Fragonard_-_The_Swing.jpg/800px-Joean_Honor%C3%A9_Fragonard_-_The_Swing.jpg',
+        src: '/images/fragonard.jpeg',
       },{
         name: 'Van Gogh - Starry Night',
-        src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/1280px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg',
+        src: '/images/vangogh.jpeg',
       },{
         name: 'Botticelli - Spring',
-        src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Botticelli-primavera.jpg/1280px-Botticelli-primavera.jpg',
+        src: '/images/botticelli.jpeg',
       },{
         name: 'Delacroix - Liberty Leading the People',
-        src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Eug%C3%A8ne_Delacroix_-_Le_28_Juillet._La_Libert%C3%A9_guidant_le_peuple.jpg/1280px-Eug%C3%A8ne_Delacroix_-_Le_28_Juillet._La_Libert%C3%A9_guidant_le_peuple.jpg',
+        src: '/images/delacroix.jpeg',
       }],
     },{
       name: 'Hoodie prints',
@@ -68,19 +68,19 @@ const loadInitData = async() => {
       startPrice: 120,
       photos: [{
         name: 'Base - white hoodie',
-        src: 'https://images.pexels.com/photos/6311503/pexels-photo-6311503.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        src: '/images/white-hoode.jpeg',
       },{
         name: 'Fragonard - Swing',
-        src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Joean_Honor%C3%A9_Fragonard_-_The_Swing.jpg/800px-Joean_Honor%C3%A9_Fragonard_-_The_Swing.jpg',
+        src: '/images/fragonard.jpeg',
       },{
         name: 'Van Gogh - Starry Night',
-        src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/1280px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg',
+        src: '/images/vangogh.jpeg',
       },{
         name: 'Botticelli - Spring',
-        src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Botticelli-primavera.jpg/1280px-Botticelli-primavera.jpg',
+        src: '/images/botticelli.jpeg',
       },{
         name: 'Delacroix - Liberty Leading the People',
-        src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Eug%C3%A8ne_Delacroix_-_Le_28_Juillet._La_Libert%C3%A9_guidant_le_peuple.jpg/1280px-Eug%C3%A8ne_Delacroix_-_Le_28_Juillet._La_Libert%C3%A9_guidant_le_peuple.jpg',
+        src: '/images/delacroix.jpeg',
       }],
     },{
       name: 'Leather jacket prints',
@@ -88,19 +88,19 @@ const loadInitData = async() => {
       startPrice: 250,
       photos: [{
         name: 'Base - white leather jacket',
-        src: 'https://images.pexels.com/photos/1070970/pexels-photo-1070970.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        src: '/images/white-jacket.jpeg',
       },{
         name: 'Fragonard - Swing',
-        src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Joean_Honor%C3%A9_Fragonard_-_The_Swing.jpg/800px-Joean_Honor%C3%A9_Fragonard_-_The_Swing.jpg',
+        src: '/images/fragonard.jpeg',
       },{
         name: 'Van Gogh - Starry Night',
-        src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/1280px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg',
+        src: '/images/vangogh.jpeg',
       },{
         name: 'Botticelli - Spring',
-        src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Botticelli-primavera.jpg/1280px-Botticelli-primavera.jpg',
+        src: '/images/botticelli.jpeg',
       },{
         name: 'Delacroix - Liberty Leading the People',
-        src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Eug%C3%A8ne_Delacroix_-_Le_28_Juillet._La_Libert%C3%A9_guidant_le_peuple.jpg/1280px-Eug%C3%A8ne_Delacroix_-_Le_28_Juillet._La_Libert%C3%A9_guidant_le_peuple.jpg',
+        src: '/images/delacroix.jpeg',
       }],
     },
   ];
